ca4b3b7dc8a5003862c1e214423988b43b3eb2f0
Lloyd <lloydmeta@gmail.com>
2016-03-17 03:52:44 +0900

    Update Webcam source code according to advice here http://stackoverflow.com/questions/32237259/opencvframegrabber-javacv-in-matlab-doesnt-work

--- src/main/scala/com/beachape/video/Webcam.scala
+++ src/main/scala/com/beachape/video/Webcam.scala
@@ -5,31 +5,31 @@ import akka.stream.actor.ActorPublisher
 import akka.stream.actor.ActorPublisherMessage.{ Cancel, Request }
 import akka.stream.scaladsl.Source
 import org.bytedeco.javacpp.opencv_core._
-import org.bytedeco.javacv.{ OpenCVFrameGrabber, Frame }
+import org.bytedeco.javacv.{ FrameGrabber, Frame }
 import org.bytedeco.javacv.FrameGrabber.ImageMode
 
 /**
- * Created by Lloyd on 2/13/16.
- */
+  * Created by Lloyd on 2/13/16.
+  */
 
 object Webcam {
 
   /**
-   * Builds a Frame [[Source]]
-   *
-   * @param deviceId device ID for the webcam
-   * @param dimensions
-   * @param bitsPerPixel
-   * @param imageMode
-   * @param system ActorSystem
-   * @return a Source of [[Frame]]s
-   */
+    * Builds a Frame [[Source]]
+    *
+    * @param deviceId device ID for the webcam
+    * @param dimensions
+    * @param bitsPerPixel
+    * @param imageMode
+    * @param system ActorSystem
+    * @return a Source of [[Frame]]s
+    */
   def source(
-    deviceId: Int,
-    dimensions: Dimensions,
-    bitsPerPixel: Int = CV_8U,
-    imageMode: ImageMode = ImageMode.COLOR
-  )(implicit system: ActorSystem): Source[Frame, Unit] = {
+              deviceId: Int,
+              dimensions: Dimensions,
+              bitsPerPixel: Int = CV_8U,
+              imageMode: ImageMode = ImageMode.COLOR
+            )(implicit system: ActorSystem): Source[Frame, Unit] = {
     val props = Props(
       new WebcamFramePublisher(
         deviceId = deviceId,
@@ -45,29 +45,44 @@ object Webcam {
     Source.fromPublisher(webcamActorPublisher)
   }
 
+  // Building a started grabber seems finicky if not synchronised; there may be some freaky stuff happening somewhere.
+  private def buildGrabber(
+                            deviceId: Int,
+                            imageWidth: Int,
+                            imageHeight: Int,
+                            bitsPerPixel: Int,
+                            imageMode: ImageMode
+                          ): FrameGrabber = synchronized {
+    val g = FrameGrabber.createDefault(deviceId)
+    g.setImageWidth(imageWidth)
+    g.setImageHeight(imageHeight)
+    g.setBitsPerPixel(bitsPerPixel)
+    g.setImageMode(imageMode)
+    g.start()
+    g
+  }
+
   /**
-   * Actor that backs the Akka Stream source
-   */
+    * Actor that backs the Akka Stream source
+    */
   private class WebcamFramePublisher(
-      deviceId: Int,
-      imageWidth: Int,
-      imageHeight: Int,
-      bitsPerPixel: Int,
-      imageMode: ImageMode
-  ) extends ActorPublisher[Frame] with ActorLogging {
+                                      deviceId: Int,
+                                      imageWidth: Int,
+                                      imageHeight: Int,
+                                      bitsPerPixel: Int,
+                                      imageMode: ImageMode
+                                    ) extends ActorPublisher[Frame] with ActorLogging {
 
     private implicit val ec = context.dispatcher
 
     // Lazy so that nothing happens until the flow begins
-    private lazy val grabber = {
-      val g = new OpenCVFrameGrabber(deviceId)
-      g.setImageWidth(imageWidth)
-      g.setImageHeight(imageHeight)
-      g.setBitsPerPixel(bitsPerPixel)
-      g.setImageMode(imageMode)
-      g.start()
-      g
-    }
+    private lazy val grabber = buildGrabber(
+      deviceId = deviceId,
+      imageWidth = imageWidth,
+      imageHeight = imageHeight,
+      bitsPerPixel = bitsPerPixel,
+      imageMode = imageMode
+    )
 
     def receive: Receive = {
       case _: Request => emitFrames()
